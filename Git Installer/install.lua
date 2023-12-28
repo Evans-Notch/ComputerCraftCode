533511ef01e206ea9a856a54409b05f6838e8347
Evan Davis <evan4489@me.com>
2023-12-27 23:41:08 -0500

    Fix command execution

--- Git Installer/install.lua
+++ Git Installer/install.lua
@@ -1,7 +1,7 @@
 -- This file is intended to be run from a floppydisk in-game
 -- This install file installs the git clone utility and a help text file to the appropriate directories
-mkdir /etc
-mkdir /etc/help
-help.setPath(/etc/help)
-cp /disk/clone.lua /etc/clone.lua
-cp /disk/clone.txt /etc/help/clone.txt
+shell.run("mkdir /etc")
+shell.run("mkdir /etc/help")
+shell.run("help.setPath(/etc/help)")
+shell.run("cp /disk/clone.lua /etc/clone.lua")
+shell.run("cp /disk/clone.txt /etc/help/clone.txt")
