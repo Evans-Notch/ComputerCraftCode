fe8d9e5f637e5c1e3011b58517d6ebcfe39177df
Lia Davis <lia@edit2014.com>
2023-12-30 13:23:07 -0500

    Fix formatting and finish levelalert script

--- Reactor/LevelAlert.lua
+++ Reactor/LevelAlert.lua
@@ -1,15 +1,23 @@
+local webhook = "" --webhook url
+local pLevel = 0 --level counter
+local target = 75 --target level
+local msg = "The polonium levels are above " .. target
+local botName = "Polonium Alert Bot"
+local botIcon = "https://upload.wikimedia.org/wikipedia/commons/b/b5/Radioactive.svg"
+
+-- Be sure to pull the discord library with: wget https://raw.githubusercontent.com/Wendelstein7/DiscordHook-CC/master/DiscordHook.lua DiscordHook.lua
 local DiscordHook = require("DiscordHook")
-local success, hook = DiscordHook.createWebhook("https://discordapp.com/api/webhooks/... (THE URL YOU GOT FROM DISCORD)")
+local success, hook = DiscordHook.createWebhook(webhook)
 if not success then
   error("Webhook connection failed! Reason: " .. hook)
 end
 
 -- If the code execution continues here, that means we have the webhook available as it didn't error.
-local pLevel = 0
-local target = 75
+
+local chest = peripheral.wrap("left")
 while pLevel < target do
-    
+    sleep(120)
+    pLevel = chest.getItemDetail(1).count
 end
-local msg = "The polonium levels are above " .. target
-local botName = "Polonium Alert Bot"
-hook.send(msg, botName, "https://upload.wikimedia.org/wikipedia/commons/b/b5/Radioactive.svg")
+
+hook.send(msg, botName, botIcon)
--- Utils/GetMethods.lua
+++ Utils/GetMethods.lua
@@ -2,6 +2,6 @@ local plist = peripheral.getNames()
 
 local file = fs.open("methods.txt", "w")
 for i=1,#plist do
-    file.write(i .. " " .. plist[i] .. " " .. peripheral.getMethods(plist[i]))
+    file.write(i .. " " .. plist[i] .. " " .. table.concat(peripheral.getMethods(plist[i]), ", "))
 end
 file.close()
